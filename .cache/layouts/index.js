8f1827f6c96b605a55900cb5be5e025c18c079f3
Clarice Bouwer <cbillowes@gmail.com>
2018-06-15 21:55:23 +0200

    Remove .cache folder

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/home/clarice/Workspace/curious-programmer/curious-programmer-oxygen/src/layouts/index.jsx"
-  import data from "/home/clarice/Workspace/curious-programmer/curious-programmer-oxygen/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
