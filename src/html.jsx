0b8f4ea709076673129e9d06cc59f3acad909250
Clarice Bouwer <cbillowes@gmail.com>
2018-06-16 22:31:48 +0200

    Add basic styling skills

--- src/html.jsx
+++ src/html.jsx
@@ -36,6 +36,7 @@ export default class HTML extends React.Component {
           />
           {this.props.headComponents}
           <link rel="shortcut icon" href={favicon} />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
           {css}
         </head>
         <body>
