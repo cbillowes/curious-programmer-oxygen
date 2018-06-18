06094b3b514e3dfaf17541d6357b39d1236287d2
Clarice Bouwer <cbillowes@gmail.com>
2018-06-18 06:09:56 +0200

    Make the heading component pretty

--- src/components/Gorget/Gorget.jsx
+++ src/components/Gorget/Gorget.jsx
@@ -4,12 +4,21 @@ import "./Gorget.css"
 class Gorget extends React.Component {
   render() {
     const title = this.props.title.toLowerCase()
-    const left = title.split(" ")[0]
-    const right = title.split(" ")[1]
+    const splitTitle = title.split(" ")
+    const left = splitTitle[0]
+    const right = splitTitle[1]
 
     return (
-      <div className="logo">
-        &#123; <span className="normal">{left}</span> {right} &#125;
+      <div className="header">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="logo">
+                &#123; <span className="normal-weight">{left}</span> {right} &#125;
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
