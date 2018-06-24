74159262270e377a9c12a382332f9352d116b8ad
Clarice Bouwer <cbillowes@gmail.com>
2018-06-24 19:10:17 +0200

    Add GA Analytic ID

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   sitePaginationLimit: 10,
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
-  googleAnalyticsID: "", // GA tracking ID.
+  googleAnalyticsID: "UA-77127562-2", // GA tracking ID.
   disqusShortname: "curious-programmer", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "Clarice Bouwer", // Username to display in the author segment.
