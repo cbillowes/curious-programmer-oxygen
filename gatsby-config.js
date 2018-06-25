fe48649a1a1c798b2d3744cabd260e2fec4df84e
Clarice Bouwer <cbillowes@gmail.com>
2018-06-25 02:00:28 +0200

    Change logo image for sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,13 +72,8 @@ module.exports = {
         display: "minimal-ui",
         icons: [
           {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
+            src: "/logos/logo.png",
+            sizes: "114x114",
             type: "image/png"
           }
         ]
