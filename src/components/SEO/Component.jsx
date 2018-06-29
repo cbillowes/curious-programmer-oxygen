ce0a53900bac91323ea707f448687031eb94c309
Clarice Bouwer <cbillowes@gmail.com>
2018-06-29 22:14:04 +0200

    Refactor render() function

--- src/components/SEO/Component.jsx
+++ src/components/SEO/Component.jsx
@@ -2,84 +2,89 @@ import React, { Component } from "react"
 import Helmet from "react-helmet"
 import config from "../../../data/SiteConfig"
 
+function getDetailsFromFrontmatter(frontmatter, postPath) {
+  return {
+    title: frontmatter.title,
+    description: frontmatter.description ? frontmatter.description : frontmatter.excerpt,
+    image: frontmatter.cover,
+    postURL: `${config.siteUrl}${config.pathPrefix}${postPath}`
+  }
+}
+
+function getDetails(postNode, postPath, postSEO) {
+  if (postSEO) {
+    return getDetailsFromFrontmatter(postNode.frontmatter, postPath)
+  } else {
+    return {
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image: config.siteLogo
+    }
+  }
+}
+
+function pushToSchemeOrgJsonLd(postSEO, blogURL, postURL, title, image, description) {
+  const schema = [
+    {
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      url: blogURL,
+      name: title,
+      alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
+    }
+  ]
+  if (postSEO) {
+    schema.push(
+      {
+        "@context": "http://schema.org",
+        "@type": "BreadcrumbList",
+        itemListElement: [
+          {
+            "@type": "ListItem",
+            position: 1,
+            item: {
+              "@id": postURL,
+              name: title,
+              image
+            }
+          }
+        ]
+      },
+      {
+        "@context": "http://schema.org",
+        "@type": "BlogPosting",
+        url: blogURL,
+        name: title,
+        alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
+        headline: title,
+        image: {
+          "@type": "ImageObject",
+          url: image
+        },
+        description
+      }
+    )
+  }
+  return schema
+}
+
 class SEO extends Component {
   render() {
     const { postNode, postPath, postSEO } = this.props
-    let title
-    let description
-    let image
-    let postURL
-    if (postSEO) {
-      const postMeta = postNode.frontmatter
-      title = postMeta.title
-      description = postMeta.description
-        ? postMeta.description
-        : postNode.excerpt
-      image = postMeta.cover
-      postURL = config.siteUrl + config.pathPrefix + postPath
-    } else {
-      title = config.siteTitle
-      description = config.siteDescription
-      image = config.siteLogo
-    }
     const realPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix
-    image = config.siteUrl + realPrefix + image
     const blogURL = config.siteUrl + config.pathPrefix
-    const schemaOrgJSONLD = [
-      {
-        "@context": "http://schema.org",
-        "@type": "WebSite",
-        url: blogURL,
-        name: title,
-        alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
-      }
-    ]
-    if (postSEO) {
-      schemaOrgJSONLD.push(
-        {
-          "@context": "http://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              item: {
-                "@id": postURL,
-                name: title,
-                image
-              }
-            }
-          ]
-        },
-        {
-          "@context": "http://schema.org",
-          "@type": "BlogPosting",
-          url: blogURL,
-          name: title,
-          alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
-          headline: title,
-          image: {
-            "@type": "ImageObject",
-            url: image
-          },
-          description
-        }
-      )
-    }
+    let { title, description, image, postURL } = getDetails(postNode, postPath, postSEO)
+    image = config.siteUrl + realPrefix + image
+    const schemaOrgJsonLd = pushToSchemeOrgJsonLd(postSEO, blogURL, postURL, title, image, description)
+
     return (
       <Helmet>
-        {/* General tags */}
         <meta name="description" content={description} />
         <meta name="image" content={image} />
-
-        {/* Schema.org tags */}
         <script type="application/ld+json">
-          {JSON.stringify(schemaOrgJSONLD)}
+          {JSON.stringify(schemaOrgJsonLd)}
         </script>
-
-        {/* OpenGraph tags */}
-        <meta property="og:url" content={postSEO ? postURL : blogURL} />
-        {postSEO ? <meta property="og:type" content="article" /> : null}
+        <meta property="og:url" content={postSEO ? postURL : blogURL} />{postSEO ? <meta property="og:type" content="article" /> : null}
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
@@ -87,8 +92,6 @@ class SEO extends Component {
           property="fb:app_id"
           content={config.siteFBAppID ? config.siteFBAppID : ""}
         />
-
-        {/* Twitter Card tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:creator"
--- src/components/SocialLinks/Component.jsx
+++ src/components/SocialLinks/Component.jsx
@@ -24,6 +24,110 @@ import {
 import config from "../../../data/SiteConfig"
 import "./SocialLinks.css"
 
+class Twitter extends Component {
+  render() {
+    const { url, title, iconSize } = this.props
+
+    return (
+      <TwitterShareButton url={url} title={title}>
+        <TwitterIcon round size={iconSize} />
+      </TwitterShareButton>
+    )
+  }
+}
+
+class LinkedIn extends Component {
+  render() {
+    const { url, title, excerpt, iconSize } = this.props
+    const filter = count => (count > 0 ? count : "")
+
+    return (
+      <LinkedinShareButton
+        url={url}
+        title={title}
+        description={excerpt}
+      >
+        <LinkedinIcon round size={iconSize} />
+        <LinkedinShareCount url={url}>
+          {count => <div className="share-count">{filter(count)}</div>}
+        </LinkedinShareCount>
+      </LinkedinShareButton>
+    )
+  }
+}
+
+class Telegram extends Component {
+  render() {
+    const { url, iconSize } = this.props
+
+    return (
+      <TelegramShareButton url={url}>
+        <TelegramIcon round size={iconSize} />
+      </TelegramShareButton>
+    )
+  }
+}
+
+class WhatsApp extends Component {
+  render() {
+    const { url, title, iconSize } = this.props
+
+    return (
+      <WhatsappShareButton url={url} title={title}>
+        <WhatsappIcon round size={iconSize} />
+      </WhatsappShareButton>
+    )
+  }
+}
+
+class Reddit extends Component {
+  render() {
+    const { url, title, iconSize } = this.props
+    const filter = count => (count > 0 ? count : "")
+
+    return (
+      <RedditShareButton url={url} title={title}>
+        <RedditIcon round size={iconSize} />
+        <RedditShareCount url={url}>
+          {count => <div className="share-count">{filter(count)}</div>}
+        </RedditShareCount>
+      </RedditShareButton>
+    )
+  }
+}
+
+class GooglePlus extends Component {
+  render() {
+    const { url, iconSize } = this.props
+    const filter = count => (count > 0 ? count : "")
+
+    return (
+      <GooglePlusShareButton url={url}>
+        <GooglePlusIcon round size={iconSize} />
+        <GooglePlusShareCount url={url}>
+          {count => <div className="share-count">{filter(count)}</div>}
+        </GooglePlusShareCount>
+      </GooglePlusShareButton>
+    )
+  }
+}
+
+class Facebook extends Component {
+  render() {
+    const { url, excerpt, iconSize } = this.props
+    const filter = count => (count > 0 ? count : "")
+
+    return (
+      <FacebookShareButton url={url} quote={excerpt}>
+        <FacebookIcon round size={iconSize} />
+        <FacebookShareCount url={url}>
+          {count => <div className="share-count">{filter(count)}</div>}
+        </FacebookShareCount>
+      </FacebookShareButton>
+    )
+  }
+}
+
 class SocialLinks extends Component {
   render() {
     const { postNode, postPath, mobile } = this.props
@@ -32,53 +136,16 @@ class SocialLinks extends Component {
     const url = config.siteUrl + realPrefix + postPath
 
     const iconSize = mobile ? 36 : 48
-    const filter = count => (count > 0 ? count : "")
 
     return (
       <div className="social-links">
-        <TwitterShareButton url={url} title={post.title}>
-          <TwitterIcon round size={iconSize} />
-        </TwitterShareButton>
-
-        <LinkedinShareButton
-          url={url}
-          title={post.title}
-          description={postNode.excerpt}
-        >
-          <LinkedinIcon round size={iconSize} />
-          <LinkedinShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
-          </LinkedinShareCount>
-        </LinkedinShareButton>
-
-        <TelegramShareButton url={url}>
-          <TelegramIcon round size={iconSize} />
-        </TelegramShareButton>
-
-        <WhatsappShareButton url={url} title={postNode.title}>
-          <WhatsappIcon round size={iconSize} />
-        </WhatsappShareButton>
-
-        <RedditShareButton url={url} title={post.title}>
-          <RedditIcon round size={iconSize} />
-          <RedditShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
-          </RedditShareCount>
-        </RedditShareButton>
-
-        <GooglePlusShareButton url={url}>
-          <GooglePlusIcon round size={iconSize} />
-          <GooglePlusShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
-          </GooglePlusShareCount>
-        </GooglePlusShareButton>
-
-        <FacebookShareButton url={url} quote={postNode.excerpt}>
-          <FacebookIcon round size={iconSize} />
-          <FacebookShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
-          </FacebookShareCount>
-        </FacebookShareButton>
+        <Twitter url={url} title={post.title} iconSize={iconSize} />
+        <LinkedIn url={url} title={post.title} iconSize={iconSize} excerpt={postNode.excerpt} />
+        <Telegram url={url} iconSize={iconSize} />
+        <WhatsApp url={url} title={post.title} iconSize={iconSize} />
+        <Reddit url={url} title={post.title} iconSize={iconSize} />
+        <GooglePlus url={url} title={post.title} iconSize={iconSize} />
+        <Facebook url={url} iconSize={iconSize} excerpt={postNode.excerpt} />
       </div>
     )
   }
