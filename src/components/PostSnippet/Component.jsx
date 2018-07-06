20dfc69cf9b09bcfe545769edbc4a4a39b37537f
Clarice Bouwer <cbillowes@gmail.com>
2018-07-06 22:49:01 +0200

    Remove unused variable

--- src/components/PostSnippet/Component.jsx
+++ src/components/PostSnippet/Component.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Link from "gatsby-link"
-import Moment from "react-moment"
 import PostTags from "../PostTags/Component"
 import PostMeta from "../PostMeta/Component"
 import "./PostSnippet.css"
