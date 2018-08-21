5d03008ed7ae6d00f3e87cd3293db35bbbbebcbf
Clarice Bouwer <cbillowes@gmail.com>
2018-08-21 06:21:35 +0200

    Update the footer

--- src/components/Footer/Component.jsx
+++ src/components/Footer/Component.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from "react"
-import Link from "gatsby-link"
-import UserLinks from "../UserLinks/Component"
 import "./Footer.css"
 
 class Footer extends Component {
   render() {
     const { config } = this.props
-    const url = config.siteRss
     const copyright = config.copyright
-    const siteUrl = config.siteUrl
-    const title = config.siteTitle
     if (!copyright) {
       return null
     }
@@ -17,13 +12,9 @@ class Footer extends Component {
       <footer className="footer">
         <div className="notice-container">
           <h4>{copyright}</h4>
-          <UserLinks config={config} labeled />
-          <Link to={url}>
-            <button>Subscribe</button>
-          </Link>
-          <div>
+          <h4>
             Born from &#123; corporate <strong>programmer</strong>&#125;
-          </div>
+          </h4>
         </div>
       </footer>
     )
