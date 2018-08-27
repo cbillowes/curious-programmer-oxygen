b3ee4f9c96e6082c3fdf35b4abcce54247c6b162
Clarice Bouwer <cbillowes@gmail.com>
2018-08-27 04:47:52 +0200

    Refactor SEO component

--- src/components/SEO/Component.jsx
+++ src/components/SEO/Component.jsx
@@ -25,98 +25,107 @@ function getSocialImage(postMeta, config) {
   return coverImage
 }
 
+function getSeo(postMeta, config, postNode, postPath, hasSEO) {
+  let fbId = (config.siteFBAppID) ? config.siteFBAppID : ""
+  let seo
+  if (hasSEO) {
+    seo = {
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image: getDefaultImage(config),
+      url: config.siteUrl + config.pathPrefix,
+      facebookId: fbId,
+      type: "article"
+    }
+  }
+  seo = {
+    title: postMeta.title,
+    description: postMeta.description ? postMeta.description : postNode.excerpt,
+    image: getSocialImage(postMeta, config),
+    twitterUser: config.userTwitter ? `@${config.userTwitter}` : "",
+    facebookId: fbId,
+    url: `${config.siteUrl}${config.pathPrefix}${postPath}`,
+    type: "website"
+  }
+  return seo
+}
+
+function getSchema(seo, config, hasSeo) {
+  const schema = [
+    {
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      url: seo.url,
+      name: seo.title,
+      alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
+    }
+  ]
+
+  if (hasSeo) {
+    schema.push(
+      {
+        "@context": "http://schema.org",
+        "@type": "BreadcrumbList",
+        itemListElement: [
+          {
+            "@type": "ListItem",
+            position: 1,
+            item: {
+              "@id": seo.url,
+              name: seo.title,
+              image: seo.image
+            }
+          }
+        ]
+      },
+      {
+        "@context": "http://schema.org",
+        "@type": "BlogPosting",
+        url: seo.url,
+        name: seo.title,
+        alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
+        headline: seo.title,
+        image: {
+          "@type": "ImageObject",
+          url: seo.image
+        },
+        description: seo.description
+      }
+    )
+  }
+  return JSON.stringify(schema)
+}
+
 class SEO extends Component {
   render() {
     const { postNode, postPath, postSEO } = this.props
-    let title
-    let description
-    let image
-    let postURL
-    let twitterUser = config.userTwitter ? `@${config.userTwitter}` : ""
-    if (postSEO) {
-      const postMeta = postNode.frontmatter
-      title = postMeta.title
-      description = postMeta.description
-        ? postMeta.description
-        : postNode.excerpt
-      image = getSocialImage(postMeta, config)
-      postURL = `${config.siteUrl}${config.pathPrefix}${postPath}`
-    } else {
-      title = config.siteTitle
-      description = config.siteDescription
-      image = getDefaultImage(config)
-    }
-    const realPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix
-    const blogURL = config.siteUrl + config.pathPrefix
-    const schemaOrgJSONLD = [
-      {
-        "@context": "http://schema.org",
-        "@type": "WebSite",
-        url: blogURL,
-        name: title,
-        alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
-      }
-    ]
-    console.log(image)
+    const postMeta = postNode.frontmatter
+    let seo = getSeo(postMeta, config, postNode, postPath, postSEO)
+    let schema = getSchema(seo, config, postSEO)
 
-    if (postSEO) {
-      schemaOrgJSONLD.push(
-        {
-          "@context": "http://schema.org",
-          "@type": "BreadcrumbList",
-          itemListElement: [
-            {
-              "@type": "ListItem",
-              position: 1,
-              item: {
-                "@id": postURL,
-                name: title,
-                image
-              }
-            }
-          ]
-        },
-        {
-          "@context": "http://schema.org",
-          "@type": "BlogPosting",
-          url: blogURL,
-          name: title,
-          alternateName: config.siteTitleAlt ? config.siteTitleAlt : "",
-          headline: title,
-          image: {
-            "@type": "ImageObject",
-            url: image
-          },
-          description
-        }
-      )
-    }
     return (
       <Helmet>
         {/* General tags */}
-        <meta name="description" content={description} />
-        <meta name="image" content={image} />
-
-        {/* Schema.org tags */}
-        <script type="application/ld+json">
-          {JSON.stringify(schemaOrgJSONLD)}
-        </script>
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={seo.image} />
 
         {/* OpenGraph tags */}
-        <meta property="og:url" content={postSEO ? postURL : blogURL} />
-        {postSEO ? <meta property="og:type" content="article" /> : null}
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
-        <meta
-          property="fb:app_id"
-          content={config.siteFBAppID ? config.siteFBAppID : ""}
-        />
+        <meta property="og:url" content={seo.url} />
+        <meta property="og:type" content={seo.type} />
+        <meta property="og:title" content={seo.title} />
+        <meta property="og:description" content={seo.description} />
+        <meta property="og:image" content={seo.image} />
+        <meta property="fb:app_id" content={seo.facebookId} />
 
         {/* Twitter Card tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={twitterUser} />
-        <meta name="twitter:creator" content={twitterUser} />
+        <meta name="twitter:site" content={seo.twitterUser} />
+        <meta name="twitter:creator" content={seo.twitterUser} />
+
+        {/* Schema.org tags */}
+        <script type="application/ld+json">
+          {schema}
+        </script>
       </Helmet>
     )
   }
